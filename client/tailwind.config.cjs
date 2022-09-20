4f06dbfecd021c5b7c1fc8abf9bcf20b20a0747b
diegocrivelaro <62905712+diegocrivelaro@users.noreply.github.com>
2022-09-19 22:58:59 -0300

    styles: add gradient

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -5,6 +5,8 @@ module.exports = {
     extend: {
       backgroundImage: {
         galaxBackground: "url('./assets/backgrounds/bg-galax.png')",
+        titleGradient:
+          "linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%), linear-gradient(0deg, #FFFFFF, #FFFFFF)",
       },
     },
   },
